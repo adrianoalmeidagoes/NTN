a671fe92cef00deec8de4985a7627af7c17953fc
adgoes <adriano-almeida.goes@capgemini.com>
2024-01-22 18:33:25 +0000

    first version

--- Code/Elevation_Analisys_UE.m
+++ Code/Elevation_Analisys_UE.m
@@ -37,7 +37,8 @@ SBand = FrequenciesTable(strcmp(FrequenciesTable.Band, 'S') & strcmp(Frequencies
 KBand = FrequenciesTable(strcmp(FrequenciesTable.Band, 'K') & strcmp(FrequenciesTable.Direction, TypeLink), :);
 KaBand = FrequenciesTable(strcmp(FrequenciesTable.Band, 'Ka') & strcmp(FrequenciesTable.Direction, TypeLink), :);
 
-Selected_Bands = { 'S: 2.1GHz', 'K: 19GHz', 'Ka: 28GHz' };
+Selected_Bands_UL = { 'S: 2.1GHz - UL', 'K: 19GHz - UL', 'Ka: 28GHz - UL' };
+Selected_Bands_DL = { 'S: 2.1GHz - DL', 'K: 19GHz - DL', 'Ka: 28GHz - DL' };
 
 S_LP = zeros(1,length(el_angle));
 K_LP = zeros(1,length(el_angle));
@@ -83,20 +84,10 @@ Ka_Throughtput = KaBand.Bandwitdh * log2(1 + dBm_to_mW(Ka_CNI));
 %**************************************************************************
 % Plot results
 %**************************************************************************
-figure();
-plot(el_angle, S_LP, '-dk', el_angle, K_LP, '--xk', el_angle, Ka_LP, '-.sk');
-legend(Selected_Bands, 'Location', 'northwest', 'FontName', 'Times New Roman', 'FontSize', 12); % Legend with font and size
-set(gca, 'GridLineStyle', '--'); % Set the grid line style to dashed
-set(gca, 'FontName', 'Times New Roman'); % Set the font type for the axes
-set(gca, 'FontSize', 12); % Set the font size for the axes
-xlabel('Satellite Elevation Angle [degree]', 'FontName', 'Times New Roman', 'FontSize', 14); % Label for the x-axis
-ylabel('Pathloss [dB]', 'FontName', 'Times New Roman', 'FontSize', 14); % Label for the y-axis
-%title(['Pathloss over Elevation Angles for different Bands in (', TypeLink, ')'], 'FontName', 'Times New Roman', 'FontSize', 14); % Title of the graph
-grid on
 
 figure();
 plot(el_angle, S_Throughtput/1e9, '-dk', el_angle, K_Throughtput/1e9, '--xk', el_angle, Ka_Throughtput/1e9, '-.sk');
-legend(Selected_Bands, 'Location', 'southeast', 'FontName', 'Times New Roman', 'FontSize', 12); % Legend with font and size
+legend(Selected_Bands_UL, 'Location', 'southeast', 'FontName', 'Times New Roman', 'FontSize', 12); % Legend with font and size
 set(gca, 'GridLineStyle', '--'); % Set the grid line style to dashed
 set(gca, 'FontName', 'Times New Roman'); % Set the font type for the axes
 set(gca, 'FontSize', 12); % Set the font size for the axes
@@ -104,6 +95,65 @@ xlabel('Satellite Elevation Angle [degree]', 'FontName', 'Times New Roman', 'Fon
 ylabel('Upperbound Throughput [Gbps]', 'FontName', 'Times New Roman', 'FontSize', 14); % Label for the y-axis
 %title(['Upperbound Throughput over Elevation Angles and Bands for (', TypeLink, ')'], 'FontName', 'Times New Roman', 'FontSize', 14); % Title of the graph
 grid on
+
+hold on
+
+%**************************************************************************
+% NTN parameters Downlink
+%**************************************************************************
+TypeLink = 'DL';
+
+% Load Band variables
+SBand = FrequenciesTable(strcmp(FrequenciesTable.Band, 'S') & strcmp(FrequenciesTable.Direction, TypeLink), :);
+KBand = FrequenciesTable(strcmp(FrequenciesTable.Band, 'K') & strcmp(FrequenciesTable.Direction, TypeLink), :);
+KaBand = FrequenciesTable(strcmp(FrequenciesTable.Band, 'Ka') & strcmp(FrequenciesTable.Direction, TypeLink), :);
+
+S_LP = zeros(1,length(el_angle));
+K_LP = zeros(1,length(el_angle));
+Ka_LP = zeros(1,length(el_angle));
+
+for i=1:length(el_angle)
+
+    A = Calculate_A_Factor(SBand.Frequency_GHz, RainFall, h0, el_angle(i), tilt_angle);
+    S_LP(i) = SatelliteFreePathloss(SBand.Frequency_GHz , h, el_angle(i)) - A;
+
+    A = Calculate_A_Factor(KBand.Frequency_GHz, RainFall, h0, el_angle(i), tilt_angle);
+    K_LP(i) = SatelliteFreePathloss(KBand.Frequency_GHz , h, el_angle(i)) - A;   
+
+    A = Calculate_A_Factor(KaBand.Frequency_GHz, RainFall, h0, el_angle(i), tilt_angle);
+    Ka_LP(i) = SatelliteFreePathloss(KaBand.Frequency_GHz , h, el_angle(i)) - A; 
+
+end
+
+%**************************************************************************
+% Calculate carrier (C) to noise (N) ratio
+%**************************************************************************
+S_N0 = ReceiveNoisePower(SBand.Bandwitdh, SBand.T);
+S_CN = (SBand.Pt + SBand.Gt + SBand.Gr + S_LP) - S_N0 - SBand.F;
+
+K_N0 = ReceiveNoisePower(KBand.Bandwitdh, KBand.T);
+K_CN = (KBand.Pt + KBand.Gt + KBand.Gr + K_LP) - K_N0 - KBand.F;  
+
+Ka_N0 = ReceiveNoisePower(KaBand.Bandwitdh, KaBand.T);
+Ka_CN = (KaBand.Pt + KaBand.Gt + KaBand.Gr + Ka_LP) - Ka_N0 - KaBand.F;  
+
+S_CI = 10 * log10(power(10, SBand.CI_int/10) + power(10, SBand.CI_ext/10) );
+K_CI = 10 * log10(power(10, KBand.CI_int/10) + power(10, KBand.CI_ext/10) );
+Ka_CI = 10 * log10(power(10, KaBand.CI_int/10) + power(10, KaBand.CI_ext/10) );
+
+S_CNI = S_CN - S_CI;
+K_CNI = K_CN - K_CI;
+Ka_CNI = Ka_CN - Ka_CI;
+
+S_Throughtput = SBand.Bandwitdh * log2(1 + dBm_to_mW(S_CNI));
+K_Throughtput = KBand.Bandwitdh * log2(1 + dBm_to_mW(K_CNI));
+Ka_Throughtput = KaBand.Bandwitdh * log2(1 + dBm_to_mW(Ka_CNI));
+
+%**************************************************************************
+% Plot results
+%**************************************************************************
+plot(el_angle, S_Throughtput/1e9, '-dr', el_angle, K_Throughtput/1e9, '--xr', el_angle, Ka_Throughtput/1e9, '-.sr');
+legend([Selected_Bands_UL, Selected_Bands_DL], 'Location', 'northwest', 'FontName', 'Times New Roman', 'FontSize', 12); % Legend with font and size
 
 
 
